354e5be9b829edf58e340b87be605464db42d9f4
James Morrissey <jammim19@gmail.com>
2026-05-01 14:04:37 +0100

    Refactoring for incorporating scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import "./reset.css";
-import "./style.css";
-import "./main.css";
-import "./split-form.css";
+import "./reset.scss";
+import "./main.scss";
+import "./split-form.scss";
 import "./js/tip-calculations.js";
 
 import { setupCounter } from "./counter.js";
