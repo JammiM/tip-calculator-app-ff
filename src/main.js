b34f289d29790d81cfe0b776219726e010af64a3
James Morrissey <jammim19@gmail.com>
2026-05-03 13:40:51 +0100

    Change CSS imports to SCSS in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./reset.css";
-import "./style.css";
-import "./main.css";
+import "./reset.scss";
+import "./style.scss";
+import "./main.scss";
 import "./split-form.css";
 import "./js/tip-calculations.js";
 
