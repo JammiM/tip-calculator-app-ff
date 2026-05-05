d2119fb9ceb62f6d7e171ca84fe2f3e54cec2156
James Morrissey <jammim19@gmail.com>
2026-05-05 13:48:38 +0100

    Removed stale code from a starter template

--- src/main.js
+++ src/main.js
@@ -2,8 +2,4 @@ import "./reset.scss";
 import "./main.scss";
 import "./split-form.scss";
 import "./js/tip-calculations.js";
-
-import { setupCounter } from "./counter.js";
 import "./axeTestingHandler.js";
-
-setupCounter(document.querySelector("#counter"));
