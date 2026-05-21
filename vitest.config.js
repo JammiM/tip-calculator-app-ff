b62770562a2872e2ff28f055c333d14ca3df8f6d
James Morrissey <jammim19@gmail.com>
2026-05-21 11:06:29 +0100

    Updated the vitest config for playwright

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,16 @@
 import config from "./vite.config";
 import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
 
 export default defineConfig({
   plugins: [...config.plugins],
   test: {
-    // globals: true, // temporarily disabled as it was conflicting with eslint
+    globals: true,
+    browser: {
+      enabled: true,
+      name: "chromium", // or 'firefox', 'webkit'
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
+    },
   },
 });
