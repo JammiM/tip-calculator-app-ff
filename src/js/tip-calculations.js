fd2d229f9f86efae9f930bf6c87d7a12dc44f78d
James Morrissey <jammim19@gmail.com>
2026-05-27 15:31:34 +0100

    Added a css grid layout for the calculated totals section

--- src/js/tip-calculations.js
+++ src/js/tip-calculations.js
@@ -226,23 +226,25 @@ export function generateForm() {
           </label>
         </div>
         <div class="calulated-totals split-form__subgrid-item">
-          <div class="split-form__calulated-totals">
-            <div class="split-form__calulated-text">
-              <p class="split-form__total-text">
-                Tip Amount
-              </p>
-              <p class="split-form__total-text--muted">/ person</p>
+          <div>
+            <div class="split-form__calulated-totals">
+              <div class="split-form__calulated-text">
+                <p class="split-form__total-text">
+                  Tip Amount
+                </p>
+                <p class="split-form__total-text--muted">/ person</p>
+              </div>
+              <p id="tip-per-person" class="split-form__final-cost">$0.00</p>
             </div>
-            <p id="tip-per-person" class="split-form__final-cost">$0.00</p>
-          </div>
-          <div class="split-form__calulated-totals">            
-            <div class="split-form__calulated-text">
-              <p class="split-form__total-text">
-                Total
-              </p>
-              <p class="split-form__total-text--muted">/ person</p>
+            <div class="split-form__calulated-totals">            
+              <div class="split-form__calulated-text">
+                <p class="split-form__total-text">
+                  Total
+                </p>
+                <p class="split-form__total-text--muted">/ person</p>
+              </div>
+              <p id="total--bill-per-person" class="split-form__final-cost">$0.00</p>
             </div>
-            <p id="total--bill-per-person" class="split-form__final-cost">$0.00</p>
           </div>
           <input type="submit" value="RESET" id="reset-button" class="split-form__reset-button" />
         </div>
