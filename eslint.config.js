a0f5e26520c2c0801498f5b7635ad4d5ca163969
James Morrissey <jammim19@gmail.com>
2026-06-02 12:42:56 +0100

    Added the 'no-shadow' rule to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
+    rules: {
+      "no-shadow": "error",
+    },
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
